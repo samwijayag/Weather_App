5be85e782c6bad81665d11ed1b2f7e46e52157f2
samwijayag <sam.wijayag@gmail.com>
2019-04-03 21:24:02 -0700

    background image changing with weather now

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let appId = 'b77e5ffa4e97bef9b34b8570c79e31d7';
 let units = "imperial";
-let searchMethod = 'zip';
+let searchMethod;
 
+
+const getSearchMethod = (searchTerm) =>{
+	if (searchTerm.length===5 & Number.parseInt(searchTerm)+ '' === searchTerm){
+		searchMethod = 'zip'
+	}else{
+		searchMethod = 'q'
+	}
+}
 
 const searchWeather = (searchTerm) => {
+	getSearchMethod(searchTerm)
 	fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
 	.then(result =>{
 		return result.json()
@@ -14,7 +23,31 @@ const searchWeather = (searchTerm) => {
 }
 
 const init = (resultFromApiCall) =>{
-	console.log(resultFromApiCall);
+	switch(resultFromApiCall.weather[0].main){
+		case 'Clear':
+			document.body.style.backgroundImage = 'url(./Images/clear.jpg)'
+			break;
+		case 'Clouds':
+			document.body.style.backgroundImage = 'url(./Images/cloudy.jpg)'
+			break;
+
+		case 'Rain':
+		case 'Drizzle':
+		case 'Mist':
+			document.body.style.backgroundImage = 'url(./Images/rain.jpg)'
+			break;
+
+		case 'Thunderstorm':
+			document.body.style.backgroundImage = 'url(./Images/storm.jpg)'
+			break;
+
+		case 'Snow':
+			document.body.style.backgroundImage = 'url(./Images/snow.jpg)'
+			break;
+
+		default:
+			break;
+	}
 }
 
 document.getElementById('searchBtn').addEventListener("click", () =>{
